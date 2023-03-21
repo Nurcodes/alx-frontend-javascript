6c9cb213e9e4587ab0b520de592486a7b0fa571b
Abdifatah Nur <deenynur21@gmail.com>
2023-03-21 22:16:10 +0300

    Finished task 6

--- 0x01-ES6_promise/tests/6-main.js
+++ 0x01-ES6_promise/tests/6-main.js
@@ -1,3 +1,7 @@
 import handleProfileSignup from '../6-final-user';
 
-console.log(handleProfileSignup('Bob', 'Dylan', 'bob_dylan.jpg'));
+const test = async () => {
+  console.log(await handleProfileSignup('Bob', 'Dylan', 'bob_dylan.jpg'));
+};
+
+test();
