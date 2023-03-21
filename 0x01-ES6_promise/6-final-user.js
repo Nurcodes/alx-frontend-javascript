ee0b32214c0c3ad8ed8d90ca712df6b6c7b93db9
Abdifatah Nur <deenynur21@gmail.com>
2023-03-21 20:15:13 +0300

    task 6 fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,7 +4,14 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const p1 = signUpUser(firstName, lastName);
   const p2 = uploadPhoto(fileName);
-  return Promise.allSettled([p1, p2]).then((res) => res.forEach((p) => p.status));
+  return Promise.allSettled([p1, p2]).then((res) => res.forEach((p) => {
+    const arr = [];
+    arr.push({
+      status: p.status,
+      value: p.value || p.reason,
+    });
+    return arr;
+  }));
 }
 
 console.log(handleProfileSignup('john', 'stockton', 'js.jpeg'));
