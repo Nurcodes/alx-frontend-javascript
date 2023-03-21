19ad4362db0bd9e71f750a82e872d5a695c6342a
Abdifatah Nur <deenynur21@gmail.com>
2023-03-21 19:27:24 +0300

    t3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -5,6 +5,6 @@ function handleProfileSignup() {
   const p2 = createUser();
   return Promise.all([p1, p2]).then((values) => {
     console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
-  }).catch(() => new Error('Signup system offline'));
+  }).catch(() => console.log('Signup system offline'));
 }
 export default handleProfileSignup;
