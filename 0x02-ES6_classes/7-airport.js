e04326484a66cd69707981391d58d5b8e951f92c
Abdifatah Nur <deenynur21@gmail.com>
2023-03-23 23:09:51 +0300

    task 7

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,16 @@
+class Airport {
+  constructor(name, code) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._name = name;
+    this._code = code;
+  }
+}
+Airport.prototype.toString = function airportToString() {
+  return `[${typeof this} ${this._code}]`;
+};
+export default Airport;
