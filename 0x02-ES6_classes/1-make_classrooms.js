8f029b656015f71c6e384d7cc6492bab5f9a8ba2
Abdifatah Nur <deenynur21@gmail.com>
2023-03-23 16:33:45 +0300

    task 0 and 1

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,10 @@
+import ClassRoom from './0-classroom';
+
+function initializeRooms() {
+  const arr = [];
+  arr.push(new ClassRoom(19));
+  arr.push(new ClassRoom(20));
+  arr.push(new ClassRoom(34));
+  return arr;
+}
+export default initializeRooms;
