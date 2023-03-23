8e309166480fa3fe18bbe377094d4c5160d89f31
Abdifatah Nur <deenynur21@gmail.com>
2023-03-23 22:25:53 +0300

    Task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,9 +1,6 @@
 class Building {
-  constructor(sqft) {
-    if (typeof sqft !== 'number') {
-      throw TypeError('Sqft must be a number');
-    }
-    if (!(Object.hasOwn(this, 'evacuationWarningMessage'))) {
+  constructor(sqft = 0) {
+    if (this.constructor !== Building && this.evacuationWarningMessage !== 'function') {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
